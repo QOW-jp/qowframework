943e1993552ab6e8898b41ed2ce1ec6db9855f2d
QOW <qow4programnormal01@gmail.com>
2023-07-09 14:25:29 +0900

    バージョン更新

--- src/qow/demo/DemoFramework.java
+++ src/qow/demo/DemoFramework.java
@@ -18,7 +18,7 @@ import java.util.NoSuchElementException;
 public class DemoFramework {
     public static void main(String[] args) {
         System.out.println("build by JDK-17.0.4");
-        System.out.println("qow_framework-1.6.1-α DemoModel\n");
+        System.out.println("qow_framework-1.6.1 DemoModel\n");
         try {
             MainSystem ms = new MainSystem();
             DemoFrameworkRule1 gr = new DemoFrameworkRule1();
