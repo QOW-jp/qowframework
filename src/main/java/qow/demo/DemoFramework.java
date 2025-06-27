a4c45f87b6f82b1615f17f829bdee40c58275c6a
QOW <qow4programnormal01@gmail.com>
2025-06-27 21:26:15 +0900

    ver.2.0.3

--- src/main/java/qow/demo/DemoFramework.java
+++ src/main/java/qow/demo/DemoFramework.java
@@ -18,7 +18,7 @@ import java.util.List;
 public class DemoFramework {
     public static void main(String[] args) {
         System.out.println("build by JDK-17.0.15");
-        System.out.println("qow_framework-2.0.2 DemoModel\n");
+        System.out.println("qow_framework-2.0.3 DemoModel\n");
         try {
             MainSystem ms = new MainSystem();
             DemoFrameworkRule1 gr = new DemoFrameworkRule1();
